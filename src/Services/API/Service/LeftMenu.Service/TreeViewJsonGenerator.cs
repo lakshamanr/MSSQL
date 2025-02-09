b2ea1793bc16ffb36cdc55bfddf10ad03549073a
lakshaman.rokade <lakshaman.rokade@edg.com.au>
2025-02-09 12:39:56 +0530

    Improve TreeViewJsonGenerator documentation and refactor
    Added XML documentation comments to the TreeViewJsonGenerator class and its methods to enhance code readability. Corrected the method name from PrepareTreeviewConfigurationApi to PrepareTreeViewConfigurationApi for consistency. Refactored the PrepareTreeViewConfigurationApi method to use an object initializer for creating the TreeViewConfiguration instance, making the code more concise and readable.

--- src/Services/API/Service/LeftMenu.Service/TreeViewJsonGenerator.cs
+++ src/Services/API/Service/LeftMenu.Service/TreeViewJsonGenerator.cs
@@ -4,28 +4,46 @@ using API.Repository.Common.Repository;
 
 namespace API.Service.LeftMenu.Service
 {
+    /// <summary>
+    /// Generates JSON structure for TreeView.
+    /// </summary>
     public class TreeViewJsonGenerator
     {
         private TreeViewConfiguration? _treeViewContext;
         private readonly IBaseRepository _baseRepository;
         private string? _currentDbName;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TreeViewJsonGenerator"/> class.
+        /// </summary>
+        /// <param name="baseRepository">The base repository.</param>
         public TreeViewJsonGenerator(IBaseRepository baseRepository)
         {
             _baseRepository = baseRepository;
-            _treeViewContext = PrepareTreeviewConfigurationApi();
+            _treeViewContext = PrepareTreeViewConfigurationApi();
         }
 
-        private TreeViewConfiguration PrepareTreeviewConfigurationApi()
+        /// <summary>
+        /// Prepares the TreeView configuration API.
+        /// </summary>
+        /// <returns>The TreeView configuration.</returns>
+        private TreeViewConfiguration PrepareTreeViewConfigurationApi()
         {
-            TreeViewConfiguration treeViewConfiguration = new TreeViewConfiguration();
-            treeViewConfiguration.ProjectName = "Project";
-            treeViewConfiguration.ServerName = _baseRepository.LoadDatabaseServerName();
+            TreeViewConfiguration treeViewConfiguration = new TreeViewConfiguration
+            {
+                ProjectName = "Project",
+                ServerName = _baseRepository.LoadDatabaseServerName()
+            };
             return treeViewConfiguration;
         }
+
+        /// <summary>
+        /// Gets the project structure asynchronously.
+        /// </summary>
+        /// <returns>The TreeView JSON structure.</returns>
         public async Task<TreeViewJson> GetProjectStructureAsync()
         {
             return await new TreeViewProjectNodeFactory(_treeViewContext, _baseRepository).CreateNodeAsync("");
-
         }
     }
 }
