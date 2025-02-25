69a104bdeeaf407d9df01dcfd9ecfa89f232e33a
lakshaman.rokade <lakshaman.rokade@edg.com.au>
2025-02-25 14:23:56 +0530

    Add new components and update styles in main page
    - Updated `main-page.component.html` to include `<app-table>`, `<app-procedure>`, and `<app-view>`.
    - Imported `TablesModule` in `main-page.component.ts` and added it to the component's imports.
    - Changed background color of `.as-split-gutter` in `styles.css` from `none` to `gray` and updated font-family.

--- src/WebClient/mssql.client/src/app/ui/main-page/main-page.component.ts
+++ src/WebClient/mssql.client/src/app/ui/main-page/main-page.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { TablesModule } from "../../table/tables.module";
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
+  imports: [TablesModule]
 })
 export class MainPageComponent implements OnInit {
 
